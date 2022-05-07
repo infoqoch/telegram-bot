85c8303f1c9b4bb177d2ef4069357b74494ad6fd
bae <infoqoch@gmail.com>
2022-05-08 08:13:07 +0900

    Initial commit

---
+++ src/test/java/infoqoch/telegrambot/TelegramBotApplicationTests.java
@@ -0,0 +1,13 @@
+package infoqoch.telegrambot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TelegramBotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
