e38b2d4a99a4913296a8052639949d340cbb0757
bae <infoqoch@gmail.com>
2022-05-11 01:54:18 +0900

    마크다운 문법을 위한 StringBuilder Wrapper 구현.

---
+++ src/main/java/infoqoch/telegrambot/util/MarkdownStringBuilder.java
@@ -0,0 +1,79 @@
+package infoqoch.telegrambot.util;
+
+import java.util.regex.Pattern;
+
+public class MarkdownStringBuilder {
+    private final StringBuilder sb = new StringBuilder();
+
+    public MarkdownStringBuilder plain(String str) {
+        sb.append(escape(str));
+        return this;
+    }
+    public MarkdownStringBuilder italic(String str) {
+        sb.append("_").append(escape(str)).append("_");
+        return this;
+    }
+    public MarkdownStringBuilder underline(String str) {
+        sb.append("__").append(escape(str)).append("__");
+        return this;
+    }
+
+    public MarkdownStringBuilder bold(String str) {
+        sb.append("*").append(escape(str)).append("*");
+        return this;
+    }
+
+    public MarkdownStringBuilder strikethrough(String str) {
+        sb.append("~").append(escape(str)).append("~");
+        return this;
+    }
+
+    public MarkdownStringBuilder code(String str) {
+        sb.append("`").append(escape(str)).append("`");
+        return this;
+    }
+
+    public MarkdownStringBuilder url(String text, String url) {
+        sb.append("[").append(escape(text)).append("](").append(url).append(")");
+        return this;
+    }
+
+    public MarkdownStringBuilder lineSeparator() {
+        sb.append(System.lineSeparator());
+        return this;
+    }
+
+    public MarkdownStringBuilder command(String command, String value) {
+        sb.append("/").append(escape(command)).append("\\_").append(escape(value.replace(" ", "_")));
+        return this;
+    }
+
+    private String escape(String str) {
+        str = str.replaceAll("[_]", "\\\\_");
+        str = str.replaceAll("[\\*]", "\\\\*");
+        str = str.replaceAll("[\\]]", "\\\\]");
+        str = str.replaceAll("[\\[]", "\\\\[");
+        str = str.replaceAll("[\\(]", "\\\\(");
+        str = str.replaceAll("[\\)]", "\\\\)");
+        str = str.replaceAll("[//)]", "\\\\/");
+        str = str.replaceAll("[~]", "\\\\~");
+        str = str.replaceAll("[`]", "\\\\`");
+        str = str.replaceAll("[>]", "\\\\>");
+        str = str.replaceAll("[<]", "\\\\<");
+        str = str.replaceAll("[#]", "\\\\#");
+        str = str.replaceAll("[\\+]", "\\\\+");
+        str = str.replaceAll("[\\-]", "\\\\-");
+        str = str.replaceAll("[=]", "\\\\=");
+        str = str.replaceAll("[\\|]", "\\\\|");
+        str = str.replaceAll("[{]", "\\\\{");
+        str = str.replaceAll("[}]", "\\\\}");
+        str = str.replaceAll("[\\.]", "\\\\.");
+        str = str.replaceAll("[!]", "\\\\!");
+        return str;
+    }
+
+    @Override
+    public String toString() {
+        return sb.toString();
+    }
+}
---
+++ src/test/java/infoqoch/telegrambot/util/MarkdownStringBuilderTest.java
@@ -0,0 +1,34 @@
+package infoqoch.telegrambot.util;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+import java.util.regex.Pattern;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class MarkdownStringBuilderTest {
+    @Test
+    void methods_test(){
+        assertThat(new MarkdownStringBuilder().italic("흘림글씨야").toString()).isEqualTo("_흘림글씨야_");
+        assertThat(new MarkdownStringBuilder().italic("흘림글씨야!").toString()).isEqualTo("_흘림글씨야\\!_");
+        assertThat(new MarkdownStringBuilder().underline("언더라인*^^*이얌").toString()).isEqualTo("__언더라인\\*^^\\*이얌__");
+        assertThat(new MarkdownStringBuilder().bold("굵게가자!").toString()).isEqualTo("*굵게가자\\!*");
+        assertThat(new MarkdownStringBuilder().bold("굵게##!|가자!").toString()).isEqualTo("*굵게\\#\\#\\!\\|가자\\!*");
+        assertThat(new MarkdownStringBuilder().strikethrough("취소선이야ㅠ").toString()).isEqualTo("~취소선이야ㅠ~");
+        assertThat(new MarkdownStringBuilder().code("<h3>코드블럭!</h3>").toString()).isEqualTo("`\\<h3\\>코드블럭\\!\\<\\/h3\\>`");
+        assertThat(new MarkdownStringBuilder().url("링크가자!", "https://naver.com").toString()).isEqualTo("[링크가자\\!](https://naver.com)");
+        assertThat(new MarkdownStringBuilder().lineSeparator().toString()).isEqualTo(System.lineSeparator());
+        assertThat(new MarkdownStringBuilder().plain("<h3>코드블럭!</h3>").toString()).isEqualTo("\\<h3\\>코드블럭\\!\\<\\/h3\\>");
+        assertThat(new MarkdownStringBuilder().command("조회","abc 123").toString()).isEqualTo("/조회\\_abc\\_123");
+    }
+
+    @Test
+    void return_this_stream(){
+        final String actual = new MarkdownStringBuilder().italic("이탈릭가자!").lineSeparator().url("가자", "https://naver.com").toString();
+        assertThat(actual).isEqualTo("_이탈릭가자\\!_"+System.lineSeparator()+"[가자](https://naver.com)");
+    }
+}
