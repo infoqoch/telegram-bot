23559265f37ee1593623654abe4f99a10720d38b
bae <infoqoch@gmail.com>
2022-06-12 15:57:36 +0900

    형식에 맞춰서 명칭을 변경함. SendDocumentRequest -> SendDocumentResponse 형태.

--- src/main/java/infoqoch/telegrambot/bot/DefaultTelegramFile.java
+++ src/main/java/infoqoch/telegrambot/bot/DefaultTelegramFile.java
@@ -1,13 +1,9 @@
 package infoqoch.telegrambot.bot;
 
 import infoqoch.telegrambot.bot.config.TelegramBotProperties;
-import infoqoch.telegrambot.bot.entity.DocumentResult;
 import infoqoch.telegrambot.bot.entity.FilePath;
-import infoqoch.telegrambot.bot.entity.Message;
 import infoqoch.telegrambot.bot.entity.Response;
 import infoqoch.telegrambot.bot.request.FilePathRequest;
-import infoqoch.telegrambot.bot.request.SendDocumentRequest;
-import infoqoch.telegrambot.bot.request.SendMessageRequest;
 import infoqoch.telegrambot.bot.response.HttpResponseWrapper;
 import infoqoch.telegrambot.util.JsonBind;
 import lombok.AccessLevel;
--- src/main/java/infoqoch/telegrambot/bot/response/SendDocumentResponse.java
+++ src/main/java/infoqoch/telegrambot/bot/response/SendDocumentResponse.java
@@ -1,5 +1,9 @@
-package infoqoch.telegrambot.bot.entity;
+package infoqoch.telegrambot.bot.response;
 
+import infoqoch.telegrambot.bot.entity.Chat;
+import infoqoch.telegrambot.bot.entity.Document;
+import infoqoch.telegrambot.bot.entity.Entities;
+import infoqoch.telegrambot.bot.entity.From;
 import lombok.Getter;
 import lombok.ToString;
 
@@ -8,7 +12,7 @@ import java.util.List;
 
 @Getter
 @ToString
-public class DocumentResult {
+public class SendDocumentResponse {
     private Long messageId;
     private Instant date;
     private From from;
