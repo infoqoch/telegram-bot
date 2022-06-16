a0c895734e061d4e8f04ac5575c4b878dc0e3396
bae <infoqoch@gmail.com>
2022-06-16 23:04:05 +0900

    bot 으로부터 properties를 가져올 수 있도록 하였음.

--- src/main/java/infoqoch/telegrambot/bot/TelegramBot.java
+++ src/main/java/infoqoch/telegrambot/bot/TelegramBot.java
@@ -1,7 +1,12 @@
 package infoqoch.telegrambot.bot;
 
+import infoqoch.telegrambot.bot.config.TelegramBotProperties;
+
 public interface TelegramBot {
     TelegramSend send();
     TelegramUpdate update();
     TelegramFile file();
+
+    /* should return immutable object */
+    TelegramBotProperties.Url url(); 
 }
