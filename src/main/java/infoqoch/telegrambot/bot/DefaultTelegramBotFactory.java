bf4688324729d5b901202b2aef2ced917bea5d93
infoqoch <infoqoch@gmail.com>
2026-01-03 22:06:46 +0900

    async로 개선

--- src/main/java/infoqoch/telegrambot/bot/DefaultTelegramBotFactory.java
+++ src/main/java/infoqoch/telegrambot/bot/DefaultTelegramBotFactory.java
@@ -6,9 +6,18 @@ import infoqoch.telegrambot.util.HttpClientHttpHandler;
 import infoqoch.telegrambot.util.HttpHandler;
 import infoqoch.telegrambot.util.JsonBind;
 
+import java.util.concurrent.Executor;
+
 public class DefaultTelegramBotFactory {
     public static TelegramBot init(String token) {
         HttpHandler httpHandler = HttpClientHttpHandler.createDefault();
+        JsonBind jsonBind = DefaultJsonBind.getInstance();
+        TelegramBotProperties properties = TelegramBotProperties.defaultProperties(token);
+        return new DefaultTelegramBot(httpHandler, jsonBind, properties);
+    }
+
+    public static TelegramBot initAsync(String token, Executor executor) {
+        HttpHandler httpHandler = HttpClientHttpHandler.createAsyncDefault(executor);
         JsonBind jsonBind = DefaultJsonBind.getInstance();
         TelegramBotProperties properties = TelegramBotProperties.defaultProperties(token);
         return new DefaultTelegramBot(httpHandler, jsonBind, properties);
